8aff205023add1e4fa03580bf105c85777b9c346
yousuf365 <33051414+yousuf365@users.noreply.github.com>
2020-03-30 17:47:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,422 @@
+<!doctype html>
+
+<html class="no-js" lang="zxx">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="" />
+<meta name="keywords" content="body guard,business,emergency,fire department,Fire Fighters,officer, police,police department, protection,safety,security,visual composer,volunteer" />
+<meta name="author" content="BDEXPERT" />
+
+<title>Muhafiz | Home</title>
+
+<!--navbar link start-->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
+<!--navbar link end-->
+
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<link rel="stylesheet" href="touchTouch/touchTouch.css">
+<link href="css/plugin.css" rel="stylesheet">
+
+<link rel="shortcut icon" type="image/ico" href="images/favicon.png" />
+<link href="style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+
+<script src="js/vendor/modernizr-2.8.3.min.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+
+</head>
+<body>
+
+<div class="preloader">
+<div class="spinner"></div>
+</div>
+
+<header class="header-area">
+<!-- navbar start-->
+<nav class="navbar navbar-inverse" style="border-style:none; margin-top:5px;">
+  <div class="container-fluid" style="background-color:white; ">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+	  <img src="images/muhafiz_services/logo1.png" title="Muhafiz" style="width:350px; height:60px; margin-top:-10px;"/>
+	  </a>
+    </div>
+   
+    <ul class="nav navbar-nav navbar-right" style="margin:25px 33px 0px 0px;">
+      <li>
+	 <div class="login">
+	 <a href="#" title="login"> <span class="glyphicon glyphicon-user"></span> Login</a>    
+	 <a href="#" title="Registration">  <span class="glyphicon glyphicon-log-in" style="margin:0px 3px 0px 15px;"></span> Registration</a>
+	  </div>
+	  </li>
+    </ul>
+  </div>
+</nav>
+<!--navbar End-->
+
+<!--menubar start-->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" 
+	  aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="background-color:white;">
+      <ul class="nav navbar-nav" id="">
+        <li class="active"><a id="menus" href="index.php" title="Home">HOME</a></li>
+		<li><a id="menus" href="#" title="About Us">PAGES</a></li>
+		<li><a id="menus" href="#" title="Services">SERVICES</a></li>
+		<li><a id="menus" href="#" title="Gallery">FEATURE</a></li>
+		<li><a id="menus" href="#" title="Team">BLOG</a></li>
+		<li><a id="menus" href="#" title="Contact us">CONTACT US</a></li>
+<!-- <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown 
+		  <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li> -->
+      </ul>
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search...">
+        </div>
+        <button class="btn btn-primary" style="" type="button">Search</button>
+      </form>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--menubar end-->
+
+
+<!--start slider-->
+ 
+	  <div class="header-single-text" style="height:600px;">
+	  
+	  <div class="crossfade">
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+	</div>
+	
+	<div class="slider-mc"><!--slider-main content start-->
+	  <div class="col-md-6" id="left-mc">
+	  <center>
+	  <a id="bam" href="https://docs.google.com/forms/d/e/1FAIpQLSdVPsLiTU9qZCZnkE40AyO4JVU0wXm8WROnhTibibKvQ9dp8w/viewform" target="_blank" class="btn default-button " title="Become A Member">Become A Member</a>
+	</center>
+	</div>
+	  
+<div class="col-md-6">
+<center>
+  <div class="login-box">
+  
+    <img src="images/avatar.png" class="avatar">
+        <h1 style="color:#383b6e;">Sign In</h1>
+            <form>
+            <p>Username</p>
+            <input type="text" name="username" placeholder="Enter Username">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="Enter Password">
+            <input type="submit" name="submit" value="Login">
+           <input type="checkbox" id="rm" value="lsRememberMe" id="rememberMe"> <label for="rememberMe">Remember me</label>
+		    <a href="#" id="fp">Forget Password?</a>    
+            </form>
+      </div>
+	  </center>
+   </div>
+	 
+</div><!-- slider-mc end-->
+
+
+	</div>
+<!--End slider-->
+
+</header>
+
+
+
+<section class="blog-area blue-bg section-padding" style="background:url(images/muhafiz_services/bg.jpg) no-repeat;">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 text-center">
+<div class="page-title">
+<h2 style="color:#cbc33d">Services</h2>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4  wow fadeIn" data-wow-delay="0.3s">
+<!-- <div class="sinlge-blog"> -->
+<div class="blog-thumb">
+<center><a href="#"><img src="images/muhafiz_services/guard-icon.png" style="height:200px; width:50%;" alt="Guard Icon"></a></center>
+</div>
+<br>
+<center><h3 style="color:#1b2848">Guards</h3></center>
+<!-- </a> -->
+<p>Quisque ipsum magna, finibus eu hendrerit in, pellentesque ut velit. Nam tempus ultrices pulvinar lacus...<!-- <a href="#" class="btn">Read more <i class="fa fa-angle-double-right "></i> --><!-- </a> -->
+</p>
+
+</div>
+<div class="col-xs-12 col-sm-4  wow fadeIn" data-wow-delay="0.3s">
+<div class="blog-thumb">
+<center><a href="#"><img src="images/muhafiz_services/car-icon.png" style="height:227px; width:55%;" alt="Car icon" ></a></center>
+</div>
+<center><h3 style="color:#1b2848">Cars</h3></center>
+<!-- </a> -->
+<p>Quisque ipsum magna, finibus eu hendrerit in, pellentesque ut velit. Nam tempus ultrices pulvinar lacus...<!-- <a href="#" class="btn">Read more <i class="fa fa-angle-double-right "></i> --><!-- </a> -->
+</p>
+</div>
+<div class="col-xs-12 col-sm-4  wow fadeIn" data-wow-delay="0.9s">
+<!-- <div class="sinlge-blog"> -->
+<div class="blog-thumb">
+<center><a href="#"><img src="images/muhafiz_services/others.png" style="height:227px; width:55%;" alt="Others" ></a></center>
+</div>
+<!-- <a href="#" title="is eating some"> -->
+<center><h3 style="color:#1b2848">Others</h3></center>
+<!-- </a> -->
+<p>Quisque ipsum magna, finibus eu hendrerit in, pellentesque ut velit. Nam tempus ultrices pulvinar lacus...<!-- <a href="#" class="btn">Read more <i class="fa fa-angle-double-right "></i> --><!-- </a> -->
+</p>
+
+</div>
+</div>
+</div>
+</section>
+<br/>
+<section  style="background-color:white">
+<div class="container">
+<div class="row">
+<div class="col-xs-12  text-center">
+<div class="page-title">
+<h2 style="color:#cbc33d">Cities</h2>
+<p style="text-align:center;margin-top:-5px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3  wow fadeIn" data-wow-delay="0.3s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/karachi.png" alt="karachi">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3  wow fadeIn" data-wow-delay="0.6s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/lahore.jpg" alt="lahore">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3  wow fadeIn" data-wow-delay="0.9s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/hyderabad.jpg" alt="hyderabad">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3  wow fadeIn" data-wow-delay="1.2s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/islamabad.jpg" alt="karachi">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3  wow fadeIn" data-wow-delay="0.3s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/hyderabad.jpg" alt="hyderabad">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3 wow fadeIn" data-wow-delay="0.6s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/lahore.jpg" alt="lahore">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3 wow fadeIn" data-wow-delay="0.9s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/islamabad.jpg" alt="islamabad">
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-3 wow fadeIn" data-wow-delay="1.2s">
+<div class="single-gallery">
+<img src="images/muhafiz_services/karachi.png" alt="Karachi" >
+<div class="gallery-icon">
+<a href="#"><i class="fa fa-plus"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section class="testimonial-area blue-bg section-padding">
+<div class="container">
+<div class="row">
+<!-- <div class="col-xs-12 text-center">
+<div class="page-title">
+<h2>Testimonials</h2>
+</div>
+</div> -->
+<div class="col-xs-12 col-sm-8 col-sm-offset-2 text-center  wow fadeIn">
+<div class="testimonial-items">
+<div class="testimonial-single-item">
+<img src="images/muhafiz_services/circle.png" alt="Michael">
+<h4 style="color:#f4ed1f">Cars</h4>
+<!-- <h5>Police</h5> -->
+</div>
+<div class="testimonial-single-item">
+
+<img src="images/muhafiz_services/circle.png" alt="">
+<h4 style="color:#f4ed1f">Cars with Guards</h4>
+<!-- <h5>Police</h5> -->
+</div>
+<div class="testimonial-single-item">
+<img src="images/muhafiz_services/circle.png" alt="">
+<h4 style="color:#f4ed1f">Cars with Guards</h4>
+<!-- <h5>Graphic Designer</h5> -->
+</div>
+<div class="testimonial-single-item">
+<img src="images/muhafiz_services/circle.png" alt="">
+<h4 style="color:#f4ed1f">Cars with Guards</h4>
+<!-- <h5>Programmer</h5> -->
+</div>
+<div class="testimonial-single-item">
+<img src="images/muhafiz_services/circle.png" alt="">
+<h4 style="color:#f4ed1f">Cars with Guards</h4>
+<!-- <h5>Police</h5> -->
+</div>
+<div class="testimonial-single-item">
+
+<img src="images/muhafiz_services/circle.png" alt="">
+<h4 style="color:#f4ed1f">Cars</h4>
+<!-- <h5>Graphic Designer</h5> -->
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="section-padding" style="background:url(images/muhafiz_services/bg2.jpg); background-position: center; background-repeat: no-repeat; background-size: cover; position: relative; height:auto;">
+<div class="row" id="pakages" style=" margin:-40px 0px 0px 40px;">
+ <center>
+    <div class="col-xs-12 col-md-12" style="color:white; height:50px;  font-weight: bold; font-size:36px;"> MEMBERSHIPS</div>
+ </center>
+</div>
+
+<div class="container" style="margin-top:40px; ">  
+
+<div class="col-md-3" style="color:white; font-weight: bold; font-size:24px; "><center><ul style="list-style-type: none;"><li>Fifteen Days</li>
+<li><button type="button" class="btn btn-default" style="background-color:#cbc33e;  border-radius:20px; border-style:none;">PKR 999/=</button></li></ul></center></div>
+
+<div class="col-md-3" style="color:white; font-weight: bold; font-size:24px; "><center><ul style="list-style-type: none;"><li>Per Month</li>
+<li><button type="button" class="btn btn-default" style="background-color:#cbc33e;  border-radius:20px; border-style:none;">PKR 999/=</button></li></ul></center></div>
+
+<div class="col-md-3" style="color:white; font-weight: bold; font-size:24px; "><center><ul style="list-style-type: none;"><li>Six Months</li>
+<li><button type="button" class="btn btn-default" style="background-color:#cbc33e;  border-radius:20px; border-style:none;">PKR 999/=</button></li></ul></center></div>
+
+<div class="col-md-3" style="color:white; font-weight: bold; font-size:24px; "><center><ul style="list-style-type: none;"><li>Per Year</li>
+<li><button type="button" class="btn btn-default" style="background-color:#cbc33e;  border-radius:20px; border-style:none;">PKR 999/=</button></li></ul></center></div>
+
+
+</div>
+</section>
+<br/>
+
+<footer class="footer-area section-padding" style="margin-top:-85px">
+<div class="container">
+<div class="row">
+
+<div class="col-xs-12 col-sm-8 col-md-4  wow fadeInLeft" data-wow-delay="0.3s">
+<div class="single-footer">
+<h3>About <span>Muhafiz</span></h3>
+<p>Organizations who hire Security Services from us are confident of hiring professionals with some sense of organizational commitment and the smile seen on their faces are real and not artificial.</p>
+
+</div>
+</div>
+
+
+
+<div class="col-xs-12 col-sm-6 col-md-3  wow fadeInLeft" data-wow-delay="0.9s">
+<div class="single-footer">
+<h3>Social Media</h3>
+
+<div class="social-list">
+<a href="#" data-toggle="tooltip" title="Facebook"><i class="fa fa-facebook"></i></a>
+<a href="#" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a>
+<a href="#" data-toggle="tooltip" title="Google plus"><i class="fa fa-google-plus"></i></a>
+<a href="#" data-toggle="tooltip" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+</div>
+<p style="margin-top:5px;">Copyright © 2020 OCLITS. All rights reserved.	</p>
+</div>
+</div>
+
+
+<div class="col-xs-12 col-sm-6 col-md-3  wow fadeInLeft" data-wow-delay="1.2s">
+<div class="single-footer">
+<h3>Info contact</h3>
+<div class="single-info">
+<a href="#" title="www.muhafiz.oclits.com"><span><i class="fa fa-globe"></i></span>www.muhafiz.oclits.com</a>
+</div>
+<div class="single-info">
+<a href="#" title="info@muhafiz.com"><span><i class="fa fa-envelope"></i></span><span class="__cf_email__" data-cfemail="452c2b232a05203d24283529206b262a28">info@muhafiz.com</span></a>
+</div>
+<div class="single-info">
+<a href="#" title="+92.21.34556331"><span><i class="fa fa-phone"></i></span>+92.21.34556331</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+
+
+<script data-cfasync="false" src="js/email-decode.min.js"></script><script src="js/vendor/jquery.min.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="js/vendor/bootstrap.min.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+
+<script src="js/jquery.circlechart.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="js/owl.carousel.min.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="touchTouch/touchTouch.jquery.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="js/scrollUp.min.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="js/plugin.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+
+<script src="js/main.js" type="624868c4e6158661928e38eb-text/javascript"></script>
+<script src="js/ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="624868c4e6158661928e38eb-|49" defer=""></script></body>
+
+<!-- Mirrored from quomodosoft.com/html/police/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 27 Feb 2020 07:09:46 GMT -->
+</html>
